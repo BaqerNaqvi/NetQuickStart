ecf3c91616410210d267b8aca496e53b544028e1
Baqer Naqvi <link2naqvi@gmail.com>
2024-03-01 16:29:48 +0500

    Setup added with logging srilog

---
+++ AONetQuickStart/Models/Human.cs
@@ -0,0 +1,21 @@
+﻿using Serilog;
+
+namespace AONetQuickStart.Models
+{
+    public class Human
+    {
+      
+
+        private readonly ILogger<Human> _logger;
+
+        public Human() {
+
+            
+        }
+
+        public void Print()
+        {
+            Log.Error("whats up");
+        }
+    }
+}
---
+++ AONetQuickStart/Program.cs
@@ -0,0 +1,34 @@
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//Add support to logging with SERILOG
+builder.Host.UseSerilog((context, configuration) =>
+    configuration.ReadFrom.Configuration(context.Configuration));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+//Add support to logging request with SERILOG
+app.UseSerilogRequestLogging();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
